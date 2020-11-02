746d8cff3e38efa181f8656bfdd6b08fb53d4527
p0isonBR <65384918+p0isonBR@users.noreply.github.com>
2020-11-02 17:36:45 -0300

    Update sms.py

--- sms.py
+++ sms.py
@@ -2,11 +2,11 @@
 import os,time,requests
 
 #Cores
-R="\033[1;31m"; B="\033[1;34m"; C="\033[1;37m"; Y="\033[1;33m"; G="\033[1;32m"; RT="\033[;0m"
+R='\033[1;31m'; B='\033[1;34m'; C='\033[1;37m'; Y='\033[1;33m'; G='\033[1;32m'; RT='\033[;0m'
 
-os.system('cls' if os.name=="nt" else 'clear')
+os.system('cls' if os.name=='nt' else 'clear')
 
-print(f"""{C}
+print(f'''{C}
                             /+osyhhhhhhyys++/
                          +oydddhhhhyyhhhhdddhy+/
                       /+yddhyyyys.josue.syyhddhs/
@@ -46,12 +46,12 @@ print(f"""{C}
      ██╔═══╝ ██║   ██║██║╚════██║██║   ██║██║╚██╗██║██╔══██╗██╔══██╗
      ██║     ╚██████╔╝██║███████║╚██████╔╝██║ ╚████║██████╔╝██║  ██║
      ╚═╝      ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝
-     {RT}{B}*t.me/p0isonBR*{RT}""")
+     {RT}{B}*t.me/p0isonBR*{RT}''')
 
 time.sleep(3)
 os.system('cls' if os.name=='nt' else 'clear')
 
-print(f"""{B}*By PoisonBR{RT}{G}
+print(f'''{B}*By PoisonBR{RT}{G}
 ███████╗███╗   ███╗███████╗      ███████╗███████╗███╗   ██╗██████╗ ███████╗██████╗ 
 ██╔════╝████╗ ████║██╔════╝      ██╔════╝██╔════╝████╗  ██║██╔══██╗██╔════╝██╔══██╗
 ███████╗██╔████╔██║███████╗█████╗███████╗█████╗  ██╔██╗ ██║██║  ██║█████╗  ██████╔╝
@@ -59,60 +59,60 @@ print(f"""{B}*By PoisonBR{RT}{G}
 ███████║██║ ╚═╝ ██║███████║      ███████║███████╗██║ ╚████║██████╔╝███████╗██║  ██║
 ╚══════╝╚═╝ v1.1╚═╝╚══════╝      ╚══════╝╚══════╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝ {C}
 Consiga suas credenciais gratis no site d7networks.com.
-{RT}""")
+{RT}''')
 
-d7="https://http-api.d7networks.com/send"
+d7='https://http-api.d7networks.com/send'
 sms={
-'from': "",
-'dlr-method': "POST",
-'dlr-url': "https://4ba60af1.ngrok.io/receive",
-'dlr': "yes",
-'dlr-level': "3"
+'from': '',
+'dlr-method': 'POST',
+'dlr-url': 'https://4ba60af1.ngrok.io/receive',
+'dlr': 'yes',
+'dlr-level': '3'
 }
 h={
-'cache-control': "no-cache"
+'cache-control': 'no-cache'
 }
-sms["username"]=input(f"{B}Username da API: {C}")
-sms["password"]=input(f"{B}Senha da API: {C}")
-sms["content"]=input(f"{B}Digite o texto a ser enviado: {C}")
+sms['username']=input(f'{B}Username da API: {C}')
+sms['password']=input(f'{B}Senha da API: {C}')
+sms['content']=input(f'{B}Digite o texto a ser enviado: {C}')
 
 try:
- np=int(input(f"{B}Para quantas pessoas deseja enviar?: {C}"))
+ np=int(input(f'{B}Para quantas pessoas deseja enviar?: {C}'))
 except (ValueError,TypeError):
- np=int(input(f"{R}Digite um valor NUMERICO valido (1-10): {C}"))
+ np=int(input(f'{R}Digite um valor NUMERICO valido (1-10): {C}'))
 except KeyboardInterrupt:
- print("Cancelado pelo usuário.")
- exit(f"{R}Ctrl+C Pressionado{C}")
+ print('Cancelado pelo usuário.')
+ exit(f'{R}Ctrl+C Pressionado{C}')
 
 try:
  while(np > 10):
-  print(f"""{Y}
+  print(f'''{Y}
 No maximo 10 pessoas por vez.
-  """); time.sleep(2)
-  np=int(input(f"{B}Digite novamente o numero de pessoas: {C}"))
- print(f"""{Y}
+  '''); time.sleep(2)
+  np=int(input(f'{B}Digite novamente o numero de pessoas: {C}'))
+ print(f'''{Y}
 ATENCAO: {C}Coloque o codigo do pais na frente do numero! ({G}+55{C})
- """)
+ ''')
 
  for sender in range(np):
-  sms['to']=input(f"{B}Dgite o numero destino: {C}")
-  print(f"""{Y}
+  sms['to']=input(f'{B}Dgite o numero destino: {C}')
+  print(f'''{Y}
 Enviando SMS...
- """)
-  send=requests.request("GET",d7,headers=h,params=sms)
+ ''')
+  send=requests.request('GET',d7,headers=h,params=sms)
   res=send.text
   feed=send.text[0:6]
-  if feed=="Succes":
-   print(f"""{G}SMS enviado com sucesso!
- """)
+  if feed=='Succes':
+   print(f'''{G}SMS enviado com sucesso!
+ ''')
   else:
-   print(f"{R}Erro ao enviar SMS, confira os dados e tente novamente.")
+   print(f'{R}Erro ao enviar SMS, confira os dados e tente novamente.')
    time.sleep(2)
-   print(f"{Y}Resposta do servidor: {C}{res}")
+   print(f'{Y}Resposta do servidor: {C}{res}')
    print()
 except KeyboardInterrupt:
  print(f'{C}Cancelado pelo usuário.')
  exit(f'{R}Ctrl+C pressionado{C}')
  
-print(f"""{C}Me acompanhe no Github: {G}https://github.com/p0isonBR
-{C}""")
+print(f'''{C}Me acompanhe no Github: {G}https://github.com/p0isonBR
+{C}''')
